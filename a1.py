3f20b570775e315db201ce6ccbef84f54ce7763d
seungyeonb <104520227+seungyeonb@users.noreply.github.com>
2022-04-29 20:12:45 +0900

    Update a1.py

--- a1.py
+++ a1.py
@@ -1,8 +1,44 @@
-print("3개의 정수값을 입력하시오")
+print("3개의 정수값을 입력하시오"
 n1 = int(input("n1의 정수값을 입력: "))
 n2 = int(input("n2의 정수값을 입력: "))
 n3 = int(input("n3의 정수값을 입력: "))
 
-total = n1 + n2 + n3;
-average = total / 3;
+total = n1+n2+n3;
+average = total/3;
 print(average)
+
+import math
+#from math import sqrt
+수 sqrt만을 가져온다.
+print("'a,b,c' 3개의 값을 입력하시오")
+a= int(input("Enter the a: ))
+b= int(input("Enter the b: "))
+c= int(input("Enter the c: "))
+
+d = math.sqrt(b*b)- (4*a*c);
+#d =  sqrt(b*b) - (4*a*c);
+x1 = (-b+d) / (2 * a);
+x2 = (-b-n) / (2 * a);
+print(x1, x2)
+
+import math
+import math
+# 근의공식 구하기
+a = int(input("a의 값을 입력하세요: "))
+b = int(input("b의 값을 입력하세요: "))
+c = int(input("c의 값을 입력하세요: "))
+
+x1 = ( (-b + (math.sqrt(math.pow(b,2) - 4 * a * c)))/2 * a)
+x2 = ( (-b + (math.sqrt(math.pow(b,2) - 4 * a * c)))/2 * a)
+
+print("x1 = ", x1)
+print("x2 = ", x2)
+
+total = 0
+counter = 1
+while counter ,= 10:
+     grade=int(input ("Enter grade: "))
+     total = grade + total
+     counter = counter + 1
+average = total / 10
+print(average)     
